5ad056438da57c903e41a11ef3610d708f2f5d23
Rodrigo Fuentes <rofuente@c2r1s1.42madrid.com>
2024-04-10 13:36:19 +0200

    M01-ex04 done

---
+++ Module-01/ex04/inc/file.hpp
@@ -0,0 +1,11 @@
+#ifndef FILE_H
+# define FILE_H
+
+# include <iostream>
+# include <fstream>
+# include <string>
+# include <cstdlib>
+
+using namespace std;
+
+#endif
--- Module-01/ex04/src/main.cpp
+++ Module-01/ex04/src/main.cpp
@@ -1,43 +1,41 @@
-#include <iostream>
-#include <fstream>
-#include <string>
+#include "../inc/file.hpp"
 
-using namespace std;
+int main(int argc, char** argv) {
+	if (argc < 4) {
+		cerr << "Uso: " << argv[0] << " archivo s1 s2" << endl;
+		return 1;
+	}
 
-int main(int argc, char* argv[]) {
-    // Verificar que se proporcionen suficientes argumentos
-    if (argc < 4) {
-        cerr << "Uso: " << argv[0] << " archivo s1 s2" << endl;
-        return 1;
-    }
+	string file = argv[1];
+	string s1 = argv[2];
+	string s2 = argv[3];
 
-    // Obtener los argumentos del archivo y los strings s1 y s2
-    string archivo = argv[1];
-    string s1 = argv[2];
-    string s2 = argv[3];
+	ifstream infile(file);
+	if (!infile) {
+		cerr << "Error abriendo el archivo: " << file << endl;
+		return 1;
+	}
 
-    // Abrir el archivo
-    ifstream entrada(archivo);
-    if (!entrada) {
-        cerr << "No se pudo abrir el archivo: " << archivo << endl;
-        return 1;
-    }
+	string out = file + ".replace";
+	ofstream outfile(out);
+	if (!outfile){
+		cerr << "Error creando el archivo de reemplazo: " << out << endl;
+		return 1;
+	}
 
-    // Leer el contenido del archivo línea por línea
-    string linea;
-    while (getline(entrada, linea)) {
-        // Reemplazar todas las ocurrencias de s1 por s2 en la línea
-        size_t pos = 0;
-        while ((pos = linea.find(s1, pos)) != string::npos) {
-            linea.replace(pos, s1.length(), s2);
-            pos += s2.length(); // Mover la posición después de la sustitución
-        }
-        // Imprimir la línea modificada
-        cout << linea << endl;
-    }
+	string line;
+	while (getline(infile, line)) {
+		size_t pos = 0;
+		while ((pos = line.find(s1, pos)) != string::npos) {
+			line = line.substr(0, pos) + s2 + line.substr(pos + s1.length());
+			pos += s2.length();
+		}
+		outfile << line << endl;
+	}
 
-    // Cerrar el archivo
-    entrada.close();
+	infile.close();
+	outfile.close();
 
-    return 0;
+	//system("leaks -q sedisforloser");
+	return 0;
 }
